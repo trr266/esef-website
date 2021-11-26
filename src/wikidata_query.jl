550fcc31420ec5308bc5f1c2ec7cf9d06b259903
Jeremiah Lewis <--get>
2021-11-26 18:26:14 +0100

    Finalize wikidata export

--- src/wikidata_query.jl
+++ src/wikidata_query.jl
@@ -1,16 +1,37 @@
 using HTTP
 using JSON
-
+using DataFrames
+using DataFrameMacros
+using Chain
 
 function query_wikidata(sparql_query_file)
-    query_string = readlines(sparql_query_file) |> join
-    # 
-    r = HTTP.request("GET", "https://query.wikidata.org/bigdata/namespace/wdq/sparql", ["Accept" => "application/sparql-results+json"], params=Dict("query"=> query_string, "format"=> "json"))
+
+    headers = ["Accept" => "application/sparql-results+json", "Content-Type"=>"application/x-www-form-urlencoded"]
+    url = "https://query.wikidata.org/bigdata/namespace/wdq/sparql"
+
+    query_string = @chain sparql_query_file readlines() join("\n") HTTP.escapeuri()
+    body = "query=$query_string"
+    r = HTTP.post(url, headers, body)
 
     d = JSON.parse(String(r.body))
 
-    return d
+    df = DataFrame()
+
+    for r in d["results"]["bindings"]
+        df1 = DataFrame(r)
+        append!(df, df1; cols=:union)
+    end
+
+    return df
 end
 
+df = query_wikidata("queries/wikidata_regulated_firms.sparql")
 
-d = query_wikidata("wikidata_regulated_firms.sparql")
+df = @chain df begin
+    @transform(:wikidata_uri = :company["value"],
+               :company_label = :companyLabel["value"])
+    @transform(:isin_id = @m :isin_value["value"])
+    @transform(:lei_id = :lei_value["value"])
+    @select(:wikidata_uri, :company_label, :isin_id, :lei_id)
+end
+
