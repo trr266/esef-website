2bc555211d743ae1441af8d30346f79f9a640a24
Jeremiah Lewis <--get>
2021-11-30 13:23:16 +0100

    Move things into functions

--- src/esef_index_viz.jl
+++ src/esef_index_viz.jl
@@ -13,59 +13,16 @@ using URIParser
 using VegaDatasets
 using VegaLite
 
+# Import helper functions
 include("wikidata_public_companies.jl")
+include("esef_xbrl_filings.jl")
 
 trr_266_colors = ["#1b8a8f", "#ffb43b", "#6ecae2", "#944664"] # petrol, yellow, blue, red
 
-xbrl_esef_index_endpoint = "https://filings.xbrl.org/index.json"
-r = HTTP.get(xbrl_esef_index_endpoint)
+df_wikidata = get_public_companies_wikidata()
+df = get_esef_xbrl_filings()
 
-# Check 200 HTTP status code
-@assert(r.status == 200)
-
-raw_data = @chain r.body begin
-    String()
-    JSON.parse()
-end
-
-df = DataFrame()
-row_names = (:key, :entity_name, :country_alpha_2, :date, :error_count, :error_codes)
-
-df_error = DataFrame()
-
-# Parse XBRL ESEF Index Object
-for (d_key, d_value) in raw_data
-    entity_name = d_value["entity"]["name"]
-    report_details = first(values(d_value["filings"]))
-
-    error_payload = report_details["errors"]
-    error_count = length(error_payload)
-    error_codes = [d["code"] for d in error_payload]
-
-    country = report_details["country"]
-    date = report_details["date"]
-
-    new_row = NamedTuple{row_names}([d_key, entity_name, country, date, error_count, error_codes])
-    push!(df, new_row)
-
-    for error_code in error_codes
-        push!(df_error, NamedTuple{(:key, :error_code)}([d_key, error_code]))
-    end
-end
-
-
-# Add in country names
-country_lookup_url = "https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/all/all.csv"
-country_lookup = @chain country_lookup_url HTTP.get(_).body CSV.read(DataFrame; normalizenames=true) @select(:country = :name, :country_alpha_2 = :alpha_2, :region)
-
-# Rename "United Kingdom of Great Britain and Northern Ireland" to "United Kingdom" for comprehensibility
-country_lookup[country_lookup.country_alpha_2 .== "GB", :country] .= "United Kingdom"
-
-europe = @chain country_lookup @subset(@m :region == "Europe"; skipmissing=true)
-
-df = @chain df begin
-    leftjoin(_, country_lookup, on=:country_alpha_2)
-end
+df
 
 pct_error_free = @chain df begin
     @transform(:error_free_report = :error_count == 0)
