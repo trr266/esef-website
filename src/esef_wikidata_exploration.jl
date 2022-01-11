57dd4441ada5d516520ad84a0132e807cda487b6
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2022-01-11 11:58:42 +0100

    Tweak script, dependencies

--- src/esef_wikidata_exploration.jl
+++ src/esef_wikidata_exploration.jl
@@ -24,14 +24,14 @@ df_wikidata_lei = enrich_wikidata_with_twitter_data(df_wikidata_lei)
 # Add further data checks
 # Check only minimal number of firms where country is missing (e.g. EU, ersatz XC/XY/XS, or incorrect 00, 23)
 @assert((@chain df_wikidata_lei @subset(ismissing(:esef_regulated)) nrow()) < 1e3)
-@assert((@chain df_wikidata_isin @subset(ismissing(:esef_regulated)) nrow()) < 10)
+# @assert((@chain df_wikidata_isin @subset(ismissing(:esef_regulated)) nrow()) < 10)
 
 # Drop firms where country is missing
-df_wikidata_isin = @chain df_wikidata_isin @subset(:esef_regulated; skipmissing=true) 
+# df_wikidata_isin = @chain df_wikidata_isin @subset(:esef_regulated; skipmissing=true) 
 
 
 # TODO: Look at this group of companies who are subject to regulation, but not available via XBRL
-@chain df_wikidata_isin @subset(:country == "Germany"; skipmissing=true)
+# @chain df_wikidata_isin @subset(:country == "Germany"; skipmissing=true)
 
 df, df_error = get_esef_xbrl_filings()
 
