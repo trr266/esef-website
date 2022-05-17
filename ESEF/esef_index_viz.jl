7d1207669baa798ef25c0668278146a5890ffac8
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2022-05-17 13:24:51 +0000

    Rearrange folder structure

--- ESEF/esef_index_viz.jl
+++ ESEF/esef_index_viz.jl
@@ -37,19 +37,19 @@ pct_error_free = @chain df begin
 end
 
 axis = (width=500, height=250, xticks=[1, 50:50:500...], xlabel="Error Count", ylabel="Filing Count", title="Errored ESEF Filings by Error Count ($(pct_error_free)% error free)")
-plt = @chain df begin 
+plt = @chain df begin
     @subset(:error_count != 0)
     data(_) * mapping(:error_count) * histogram(bins=range(1, 500, length=50)) * visual(color=trr_266_colors[1])
 end
 
 fg1 = draw(plt; axis)
 
-save("figs/esef_error_hist.svg", fg1, px_per_unit = 3)
+save("figs/esef_error_hist.svg", fg1, px_per_unit=3)
 
 axis = (width=500,
     height=500,
     xticks=[1, 50:50:500...],
-    ylabel="Log1p Error Count", 
+    ylabel="Log1p Error Count",
     xlabel="Log1p Twitter Follower Count (Cumulative)",
     title="ESEF Filing Errors by Twitter Follower Count")
 
@@ -61,7 +61,7 @@ end
 
 fg1 = draw(plt; axis)
 
-save("figs/esef_errors_followers.svg", fg1, px_per_unit = 3)
+save("figs/esef_errors_followers.svg", fg1, px_per_unit=3)
 
 world110m = dataset("world-110m")
 
@@ -72,7 +72,7 @@ country_rollup = @chain df begin
     @combine(:report_count = length(:country))
     @transform(:report_count = coalesce(:report_count, 0))
 end
-    
+
 # jscpd:ignore-start
 
 # First is for web, second for poster
@@ -82,47 +82,47 @@ for map_height in map_heights
 
     map_output = map_height[1]
     map_height = map_height[2]
-    fg2a = @vlplot(width=500, height=map_height, title={text="ESEF Report Availability by Country", subtitle="(XBRL Repository)"})
+    fg2a = @vlplot(width = 500, height = map_height, title = {text = "ESEF Report Availability by Country", subtitle = "(XBRL Repository)"})
 
-    fg2b = @vlplot(width=500, height=map_height,
-        mark={:geoshape, stroke=:white, fill=:lightgray},
-        data={
-            url=world_geojson,
-            format={
-                type=:topojson,
-                feature=:countries
+    fg2b = @vlplot(width = 500, height = map_height,
+        mark = {:geoshape, stroke = :white, fill = :lightgray},
+        data = {
+            url = world_geojson,
+            format = {
+                type = :topojson,
+                feature = :countries
             }
         },
-        projection={
-            type=:azimuthalEqualArea,
-            scale=525,
-            center=[15, 53],
+        projection = {
+            type = :azimuthalEqualArea,
+            scale = 525,
+            center = [15, 53],
         },
     )
 
-    fg2c = @vlplot(width=500, height=map_height,
-        mark={:geoshape, stroke=:white},
-        data={
-            url=world_geojson,
-            format={
-                type=:topojson,
-                feature=:countries
+    fg2c = @vlplot(width = 500, height = map_height,
+        mark = {:geoshape, stroke = :white},
+        data = {
+            url = world_geojson,
+            format = {
+                type = :topojson,
+                feature = :countries
             }
         },
-        transform=[{
-            lookup="properties.name",
-            from={
-                data=(@chain country_rollup @subset(:report_count > 0)),
-                key=:country,
-                fields=["report_count"]
+        transform = [{
+            lookup = "properties.name",
+            from = {
+                data = (@chain country_rollup @subset(:report_count > 0)),
+                key = :country,
+                fields = ["report_count"]
             }
         }],
-        projection={
-            type=:azimuthalEqualArea,
-            scale=525,
-            center=[15, 53],
+        projection = {
+            type = :azimuthalEqualArea,
+            scale = 525,
+            center = [15, 53],
         },
-        fill={"report_count:q", axis={title="Report Count"}, scale={range=["#ffffff", trr_266_colors[2]]}},
+        fill = {"report_count:q", axis = {title = "Report Count"}, scale = {range = ["#ffffff", trr_266_colors[2]]}},
     )
 
     fg2 = (fg2a + fg2b + fg2c)
@@ -138,69 +138,69 @@ for map_height in map_heights
         fg2.params["config"] = ("view" => ("stroke" => "transparent")) # remove grey border
         fg2.params["layer"][2]["encoding"]["fill"]["legend"] = nothing # drop legend
         fg2.params["title"] = nothing
-        
+
         save("figs/esef_country_availability_map_poster.svg", fg2)
     end
 end
 
-fg2_bar = (@chain country_rollup @subset(:report_count > 0))  |>
-    @vlplot({:bar, color=trr_266_colors[1]}, width=500, height=300,
-        x={"country:o", title=nothing, sort="-y"},
-        y={:report_count, title="Report Count"},
-        title={
-            text="ESEF Report Availability by Country",
-            subtitle="(XBRL Repository)"
-            },
-        )
+fg2_bar = (@chain country_rollup @subset(:report_count > 0)) |>
+          @vlplot({:bar, color = trr_266_colors[1]}, width = 500, height = 300,
+    x = {"country:o", title = nothing, sort = "-y"},
+    y = {:report_count, title = "Report Count"},
+    title = {
+        text = "ESEF Report Availability by Country",
+        subtitle = "(XBRL Repository)"
+    },
+)
 save("figs/esef_country_availability_bar.svg", fg2_bar)
 
 esef_year_url = "https://raw.githubusercontent.com/trr266/esef/main/data/esef_mandate_overview.csv"
 esef_year_df = @chain esef_year_url HTTP.get(_).body CSV.read(DataFrame; normalizenames=true)
 
-fg3a = @vlplot(width=500, height=300, title={text="ESEF Mandate by Country", subtitle="(Based on Issuer's Fiscal Year Start Date)"})
+fg3a = @vlplot(width = 500, height = 300, title = {text = "ESEF Mandate by Country", subtitle = "(Based on Issuer's Fiscal Year Start Date)"})
 
 fg3b = @vlplot(
-    mark={:geoshape, stroke=:white, fill=:lightgray},
-    data={
-        url=world_geojson,
-        format={
-            type=:topojson,
-            feature=:countries
+    mark = {:geoshape, stroke = :white, fill = :lightgray},
+    data = {
+        url = world_geojson,
+        format = {
+            type = :topojson,
+            feature = :countries
         }
     },
-    projection={
-        type=:azimuthalEqualArea,
-        scale=525,
-        center=[15, 53],
+    projection = {
+        type = :azimuthalEqualArea,
+        scale = 525,
+        center = [15, 53],
     },
 )
 
 fg3c = @vlplot(
-    mark={:geoshape, stroke=:white},
-    width=500, height=300,
-    data={
-        url=world_geojson,
-        format={
-            type=:topojson,
-            feature=:countries
+    mark = {:geoshape, stroke = :white},
+    width = 500, height = 300,
+    data = {
+        url = world_geojson,
+        format = {
+            type = :topojson,
+            feature = :countries
         }
     },
-    transform=[{
-        lookup="properties.name",
-        from={
-            data=esef_year_df,
-            key=:Country,
-            fields=["Mandate_Affects_Fiscal_Year_Beginning"]
-        }
-    },
-    {filter="isValid(datum.Mandate_Affects_Fiscal_Year_Beginning)"}
+    transform = [{
+            lookup = "properties.name",
+            from = {
+                data = esef_year_df,
+                key = :Country,
+                fields = ["Mandate_Affects_Fiscal_Year_Beginning"]
+            }
+        },
+        {filter = "isValid(datum.Mandate_Affects_Fiscal_Year_Beginning)"}
     ],
-    projection={
-        type=:azimuthalEqualArea,
-        scale=525,
-        center=[15, 53],
+    projection = {
+        type = :azimuthalEqualArea,
+        scale = 525,
+        center = [15, 53],
     },
-    color={"Mandate_Affects_Fiscal_Year_Beginning:O", axis={title="Mandate Starts"}, scale={range=trr_266_colors}},
+    color = {"Mandate_Affects_Fiscal_Year_Beginning:O", axis = {title = "Mandate Starts"}, scale = {range = trr_266_colors}},
 )
 
 fg3 = (fg3a + fg3b + fg3c)
@@ -217,12 +217,12 @@ df_error_count = @chain df_error_wide begin
     @combine(:error_count = length(:error_code))
 end
 
-fg_error_freq_bar = df_error_count  |>
-    @vlplot({:bar, color=trr_266_colors[1]}, width=500, height=500,
-        y={"error_code:o", title="Error Code", sort="-x"},
-        x={"error_count", title="Error Count"},
-        title={text="ESEF Error Frequency", subtitle="(XBRL Repository)"}
-    )
+fg_error_freq_bar = df_error_count |>
+                    @vlplot({:bar, color = trr_266_colors[1]}, width = 500, height = 500,
+    y = {"error_code:o", title = "Error Code", sort = "-x"},
+    x = {"error_count", title = "Error Count"},
+    title = {text = "ESEF Error Frequency", subtitle = "(XBRL Repository)"}
+)
 save("figs/esef_error_type_freq_bar.svg", fg_error_freq_bar)
 
 
@@ -232,12 +232,12 @@ df_error_country = @chain df_error_wide begin
 end
 
 fg_error_country_heatmap = df_error_country |>
-    @vlplot(:rect, width=500, height=500,
-        x={"country:o", title=nothing},
-        y={"error_code:o", title="Error Code"},
-        color={:error_count, title="Error Count", scale={range=["#ffffff", trr_266_colors[2]]}},
-        title="Error Frequency by Country and Type"
-    )
+                           @vlplot(:rect, width = 500, height = 500,
+    x = {"country:o", title = nothing},
+    y = {"error_code:o", title = "Error Code"},
+    color = {:error_count, title = "Error Count", scale = {range = ["#ffffff", trr_266_colors[2]]}},
+    title = "Error Frequency by Country and Type"
+)
 save("figs/esef_error_country_heatmap.svg", fg_error_country_heatmap)
 
 df_country_date = @chain df begin
@@ -246,19 +246,19 @@ df_country_date = @chain df begin
 end
 
 fg_country_date = df_country_date |>
-    @vlplot(:rect, width=500, height=500,
-        y={"country:o", title=nothing},
-        x={"date:o", title="Date"},
-        color={"report_count:q", title="Report Count", scale={range=["#ffffff", trr_266_colors[2]]}},
-        title="Report Publication by Country and Date"
-    )
+                  @vlplot(:rect, width = 500, height = 500,
+    y = {"country:o", title = nothing},
+    x = {"date:o", title = "Date"},
+    color = {"report_count:q", title = "Report Count", scale = {range = ["#ffffff", trr_266_colors[2]]}},
+    title = "Report Publication by Country and Date"
+)
 
 fg_date_bar = df_country_date |>
-    @vlplot({:bar, color=trr_266_colors[2]}, width=500, height=100,
-        y={"sum(report_count)", title="Report Count"},
-        x={"date:o", title="Date"},
-        title="Report Publication by Date"
-    )
+              @vlplot({:bar, color = trr_266_colors[2]}, width = 500, height = 100,
+    y = {"sum(report_count)", title = "Report Count"},
+    x = {"date:o", title = "Date"},
+    title = "Report Publication by Date"
+)
 
 fg_date_composite = [fg_date_bar; fg_country_date]
 save("figs/esef_publication_date_composite.svg", fg_date_composite)
